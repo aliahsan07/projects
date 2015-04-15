a2e8f692484e7090a4ba3e76c9cea704cf3c26b0
shieldal <higgs.alec@gmail.com>
2015-04-14 19:09:33 -0700

    Implement smithyEffect

--- shieldal/dominion/dominion.c
+++ shieldal/dominion/dominion.c
@@ -8,6 +8,7 @@
 int councilRoomEffect(int, struct gameState*, int);
 int mineEffect(int, int, int, struct gameState*, int);
 int remodelEffect(int, int, int, struct gameState*, int);
+int smithyEffect(int, struct gameState*, int);
 
 int compare(const void* a, const void* b) {
   if (*(int*)a > *(int*)b)
@@ -1349,5 +1350,20 @@ int remodelEffect(int currentPlayer, int choice1, int choice2, struct gameState*
 
       return 0;
 }
+
+int smithyEffect(int currentPlayer, struct gameState* state, int handPos)
+{
+      int i;
+      //+3 Cards
+      for (i = 0; i < 3; i++)
+	{
+	  drawCard(currentPlayer, state);
+	}
+			
+      //discard card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+      return 0;
+}
+
 //end of dominion.c
 
