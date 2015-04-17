55679c7549d06755c094c5751e09775933d2a1ba
Rachmaninov13 <gassa@onid.oregonstate.edu>
2015-04-16 23:33:39 -0700

    Updated with errors. Added refactor.txt. -Previously, established codebase with abstraction of 5 functions.

--- gassa/dominion/dominion.c
+++ gassa/dominion/dominion.c
@@ -660,7 +660,7 @@ int cardAdventurer(struct gameState *state)
 	  drawntreasure++;
 	else{
 	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+	  state->handCount[currentPlayer]++; //this should just remove the top card (the most recently drawn one).
 	  z++;
 	}
       }
@@ -757,7 +757,7 @@ int cardGardens(void)
 {
 	return -1;
 }
-int cardMine(int choice1, int choice2, struct gameState *state, int handPos)
+int cardMine(int choice2, int choice1, struct gameState *state, int handPos)
 {
 	int i;
 	int j;
