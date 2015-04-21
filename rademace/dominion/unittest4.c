eac86498c999eafe82c3b35d43c44790b55b6fc5
EliasRademacher <rademace@onid.oregonstate.edu>
2015-04-21 13:20:12 -0700

    Write tests in unittest4.c for shuffle() function

--- rademace/dominion/unittest4.c
+++ rademace/dominion/unittest4.c
@@ -5,4 +5,52 @@
  *      Author: Elias
  */
 
+#include <stdlib.h>
+#include <time.h>
+#include "assert.h"
+#include "dominion.h"
+#include "dominion.c"
+#include "rngs.h"
+#include "rngs.c"
 
+
+int main(int argc, char argv[]) {
+	
+	int player, i, card, numMatches, deckSize;
+	struct gameState* state;
+	struct gameState* savedState;
+		
+	player = 0;
+	state = newGame();
+	savedState = newGame();
+	state->deckCount[player] = 0;
+	assert(shuffle(player, state) == -1);
+	
+	for (deckSize = 10; deckSize < MAX_DECK; deckSize = deckSize + 10) {
+		state->deckCount[player] = deckSize;
+		
+		srand(time(NULL));
+		for (i = 0; i < deckSize; i++) {
+			card = rand() % 10;
+			state->deck[player][i] = card;
+			savedState->deck[player][i] = card;
+		}
+		
+		assert(shuffle(player, state) == 0);
+		numMatches = 0;
+		for (i = 0; i < deckSize; i++) {
+			if (state->deck[player][i] == savedState->deck[player][i])
+				numMatches++;
+		}
+		
+		assert(numMatches < deckSize / 2);
+		//printf("Number of matches: %i\n", numMatches);
+	}
+	
+	
+	free(state);
+	
+	printf("shuffle() passed all tests\n");
+	
+	return 1;
+}
