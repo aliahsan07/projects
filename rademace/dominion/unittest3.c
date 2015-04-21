5417c5238dd2d4fa0da41b818df17035eb74082d
EliasRademacher <rademace@onid.oregonstate.edu>
2015-04-21 11:59:05 -0700

    Write tests in unittest3.c for kindomCards()

--- rademace/dominion/unittest3.c
+++ rademace/dominion/unittest3.c
@@ -5,4 +5,42 @@
  *      Author: Elias
  */
 
+#include "assert.h"
+#include "dominion.c"
+#include "dominion.h"
+#include "rngs.c"
+#include "rngs.h"
 
+int main(int argc, char argv[]) {
+
+	int* k = kingdomCards(
+		cutpurse,
+		embargo,
+		outpost,
+		minion,
+		steward,
+		tribute,
+		adventurer,
+		council_room,
+		gardens,
+		mine);
+		
+	assert(k != NULL);	
+	assert(k[0] == cutpurse);
+	assert(k[1] == embargo);
+	assert(k[2] == outpost);
+	assert(k[3] == minion);
+	assert(k[4] == steward);
+	assert(k[5] == tribute);
+	assert(k[6] == adventurer);
+	assert(k[7] == council_room);
+	assert(k[8] == gardens);
+	assert(k[9] == mine);
+	
+	free(k);
+	
+	printf("kindomCards() passed all tests\n");
+	
+	return 1;
+
+}
