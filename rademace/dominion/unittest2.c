bb0b13cb29fc01b0749ff6f4102548eb32e0dc98
EliasRademacher <rademace@onid.oregonstate.edu>
2015-04-21 11:36:18 -0700

    Write tests in unittest2.c for newGame()

--- rademace/dominion/unittest2.c
+++ rademace/dominion/unittest2.c
@@ -4,5 +4,40 @@
  *  Created on: Apr 20, 2015
  *      Author: Elias
  */
+#include <dominion.c>
+#include <dominion.h>
+#include <limits.h>
+#include <stdlib.h>
+#include "assert.h"
+#include "rngs.h"
+#include "rngs.c"
 
+#define TEST_INT INT_MAX
+#define NUM_PLAYERS 5
 
+int main(int argc, char argv[]) {
+	
+	int* i;
+	int j;
+	struct gameState* g;
+	g = newGame();
+	
+	assert(g != NULL);
+	
+	g->numPlayers = NUM_PLAYERS;
+	assert(NUM_PLAYERS == g->numPlayers);
+	
+	for (j = 0, i = (int*) g; j < sizeof(struct gameState) / sizeof(int); j++, i++) {
+		*i = TEST_INT;
+	}
+	
+	for (j = 0, i = (int*) g; j < sizeof(struct gameState) / sizeof(int); j++, i++) {
+		assert(*i == TEST_INT);
+	}
+	
+	free(g);
+	
+	printf("newGame() passed all tests\n");
+		
+    return 1;	
+}
