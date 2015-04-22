50c353665ee28f8dc12aecdfe8464a72964d248d
martich2 <martich2@ondi.orst.edu>
2015-04-22 15:33:54 -0700

    last commit was for this file.

---
+++ martich2/dominion/unittest1.c
@@ -0,0 +1,49 @@
+/*
+ * unittest1.c
+ *
+ * test of compare compare(const void* a, const void* b)
+ *
+ *  Created on: Apr 22, 2015
+ *      Author: martich2
+ */
+
+#include <stdio.h>
+#include <assert.h>
+
+/* prototype here to allow compiling */
+int compare(const void* a, const void* b);
+
+int main(void)
+{
+	int a = 5;
+	int b = 2;
+	int result;
+
+	int* ptr_a = &a;
+	int* ptr_b = &b;
+
+	 /* a > b -> 1 */
+	result = compare(ptr_a,ptr_b);
+	assert(result == 1);
+	printf("Test 1: A=%d is > B=%d passes\n", a, b);
+
+	/* a < b -> -1 */
+	b = 7;
+	result = compare(ptr_a,ptr_b);
+	assert(result == -1);
+	printf("Test 2: A=%d is < B=%d passes\n", a, b);
+
+	/* a = b -> 0 */
+	a = 7;
+	result = compare(ptr_a,ptr_b);
+	assert(result == 0);
+	printf("Test 3: A=%d is = B=%d passes\n", a, b);
+
+	/* does it handles null data? */
+	ptr_a = NULL;
+	result = compare(ptr_a,ptr_b);
+	// none shall pass, seg fault.
+	printf("Test 4: A=NULL is = B=%d passes\n", b);
+
+	return 0;
+}
