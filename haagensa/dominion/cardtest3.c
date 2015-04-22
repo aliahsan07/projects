ecbdec733c1572efe9c77d4ca04192200c755c72
ClownBabyJunior <john.calvin.coolidge@gmail.com>
2015-04-22 10:35:39 -0700

    Added card test 3 for council room

--- haagensa/dominion/cardtest3.c
+++ haagensa/dominion/cardtest3.c
@@ -13,5 +13,43 @@
 
 int main (){
 
+
+  struct gameState state;
+  srand(time(NULL));
+  printf("\n !! Card Test 3 !! \n");
+  printf("Council Card Testing: Begin! \n");
+  int startSeed = rand();
+  
+  int k[10]={adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+  initializeGame(3,k,startSeed,&state);
+  int preCount = state.numBuys;
+
+  int curPlayer = 1;
+  int curHand = 1;  
+  int i = 0;
+
+  //Took switch-case function and pulled it out to create test
+  //from council case
+  //perform a quick player draw test
+  for (i = 0; i < 5; i++) {
+    drawCard(curPlayer, &state);
+  }
+
+  state.numBuys++;
+			
+  //Each other player draws a card for council room
+  for (i = 0; i < state.numPlayers; i++){
+    if ( i != curPlayer ){
+      drawCard(i, &state);
+    }
+  }
+  
+  discardCard(curHand, curPlayer, &state, 0);  
+
+  assert(state.numBuys > preCount);
+  printf("Assertions: Passed\n");
+
+  return 0;
+
 }
     
