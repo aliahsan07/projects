c04d49e20d4cb485828adc4505968ece3399c21b
MrFengShui <luansongjian@outlook.com>
2015-04-21 20:45:21 -0700

    multiple add

--- luans/dominion/cardtest1.c
+++ luans/dominion/cardtest1.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+
+#include "dominion.h"
+#include "rngs.h"
 
 int main(int argc, char *argv[])
 {
+	struct gameState *game = newGame();
+	int player = 0;
+	int pos = 4;
+	
+	printf("Starting to test council_room card......\n");
+	
+	int result = func_council_room(player, pos, game);
+	assert(result == 0);
+	
 	return EXIT_SUCCESS;
 }
--- luans/dominion/cardtest2.c
+++ luans/dominion/cardtest2.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+
+#include "dominion.h"
+#include "rngs.h"
 
 int main(int argc, char *argv[])
 {
