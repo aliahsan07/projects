37843b14f1f6e16f29952d9ee948f80532c75b8d
ClownBabyJunior <john.calvin.coolidge@gmail.com>
2015-04-22 09:48:55 -0700

    finished cardTest2 for Smithy

--- haagensa/dominion/cardtest2.c
+++ haagensa/dominion/cardtest2.c
@@ -13,5 +13,26 @@
 
 int main(){
 	
+  printf("\n !! Card Test 2 !! \n");
+  srand(time(NULL));
+  printf(" Smithy Card Testing: Begin! \n");
+  struct gameState state;
+  int startSeed = rand();
+  int hand = 1;
+  int p = 3;
+  int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
 
+  printf("Status: Starting game. \n");
+  //set turnCount and try for different turn values
+  int turnCount = (rand() % 4) + 1;
+  for(turnCount = 1; turnCount < 4; turnCount++){
+    printf("Turn for test: %d\n", turnCount);
+    initializeGame(p, k, startSeed, &state);
+    int preCount = state.handCount[turnCount];
+    int returnVal = smithyRefactor(turnCount, &state, hand);
+    assert(returnVal == 0);
+    assert(preCount + 2 == state.handCount[turnCount]);
+    printf("Assertions passed for turn %d!\n", turnCount);
+  }  
+  return 0;
 }
