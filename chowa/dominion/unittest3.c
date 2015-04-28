08c1c70ad1579a0c6473544f4a20df401e91c270
anitacchow <chowa@onid.oregonstate.edu>
2015-04-27 23:05:47 -0700

    unittest1 done

---
+++ chowa/dominion/unittest3.c
@@ -0,0 +1,19 @@
+/*
+Name: unittest3.c
+
+Purpose: 
+ */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+printf("Testing \n");
+
+struct gameState *game = newGame();
+
+printf(" Testing complete\n");
+}
