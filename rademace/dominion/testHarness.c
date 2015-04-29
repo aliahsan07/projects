78145197117d5c9a38c6c13c60655ec64bfc5f95
Elias Rademacher <rademace@os-class.engr.oregonstate.edu>
2015-04-29 14:19:49 -0700

    Add --coverage to Makefile flags

---
+++ rademace/dominion/testHarness.c
@@ -0,0 +1,17 @@
+#include cardtest1.c
+#include cardtest2.c
+#include cardtest3.c
+#include cardtest4.c
+#include unittest1.c
+#include unittest2.c
+#include unittest3.c
+#include unittest4.c
+
+int main(){
+	
+	
+	
+	
+	
+return 1;	
+}
