c0738935afc23d07d4cbb0a83a8c9bc224b5ead0
martich2 <martich2@ondi.orst.edu>
2015-05-07 16:43:47 -0700

    Got all the code done for adventurer test. Have not run gcov on it yet.

--- martich2/dominion/randomtestadventurer.c
+++ martich2/dominion/randomtestadventurer.c
@@ -10,12 +10,11 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <assert.h>
 
 #include "dominion.h"
 #include "dominion_helpers.h"
 
-#define MAX_TESTS 100
+#define MAX_TESTS 200
 
 /**
  * Prints a fail message or "passes" based on passed in expression.
@@ -30,19 +29,20 @@ int main()
             sea_hag, tribute, smithy };
 
     int i = 0, card = 0, player = 0, players = 0, seed = 0;
-    int handCount = 0, deckCount = 0;
+    int the_test = 0, handCount = 0, deckCount = 0, discardCount = 0;
+    int tmpCard = 0, treasureCount = 0;
     struct gameState *aGame = NULL;
 
+    srand(time(NULL));          // init libc random seed
     for (i = 0; i < MAX_TESTS; i++)
     {
         // set up game state
         aGame = newGame();          // allocate new memory
-        srand(time(NULL));          // init libc random seed
         seed = rand();              // any value will do
         players = (rand() % 3) + 2; // 2 to 4 players
         player = rand() % players;  // randomly pick a player to play
         initializeGame(players, k, seed, aGame);
-        aGame->whoseTurn = player;  // set player asss active player
+        aGame->whoseTurn = player;  // set player as active player
 
         // set up player hand to between a minimum of 5 and max hand;
         do
@@ -53,6 +53,7 @@ int main()
         aGame->handCount[player] = handCount;
 
         // set up player deck deck size at minimum 10;
+        /* !! SEG FAULT if deck is 0 !! */
         do
         {
             deckCount = rand() % (MAX_DECK + 1);
@@ -75,27 +76,69 @@ int main()
             aGame->deck[player][card] = rand() % NUMBER_OF_CARDS;
         }
 
-        // run tests
-        cardEffect(adventurer, 0, 0, 0, aGame, 0, NULL);
-        //assert ... ?
+        // are there two treasure cards in deck?
+        for (card = 0; card < deckCount; card++)
+        {
+            tmpCard = aGame->deck[player][card];
+            if (tmpCard == gold || tmpCard == silver ||tmpCard == copper)
+                treasureCount++;
 
+            if (treasureCount >= 2) break;
+        }
+
+        printf("Test %d: ", i);
+
+        // run a different tests based on the number of treasure cards in deck
+        if (treasureCount >= 2)
+        {
+            discardCount = aGame->discardCount[player];
+            cardEffect(adventurer, 0, 0, 0, aGame, 0, NULL);
+
+            // adventurer draws two treasure cards
+            the_test = (handCount + 2 == aGame->handCount[player]);
+            printf("Two Treasure cards drawn...");
+            test(the_test, "Did not draw two cards");
+        }
+        else if (treasureCount == 1)
+        {
+            cardEffect(adventurer, 0, 0, 0, aGame, 0, NULL);
+            the_test = (handCount + 1 == aGame->handCount[player]);
+            printf("One Treasure cards drawn...");
+            test(the_test, "Did not draw two cards");
+        }
+        else
+        {
+            printf("Zero treasure cards drawn...");
+            the_test = (handCount == aGame->handCount[player]);
+            cardEffect(adventurer, 0, 0, 0, aGame, 0, NULL);
+            test(the_test, "Hand size changed when it should not");
+        }
+
+        // adventurer discards drawn cards that are not treasure
+        // if the one treasure card is the first card in the deck, no discarding
+        the_test = (discardCount <= aGame->discardCount[player]);
+        printf(" ... Discard pile grew or unchanged ...");
+        test(the_test, "Did not discard any cards");
+
+        printf("\n");
 
         //clean up
         free(aGame);
         player = 0, players = 0, seed = 0, handCount = 0, deckCount = 0;
+        the_test = 0, treasureCount = 0, tmpCard = 0;
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 void test(int expr, char *msg)
 {
     if (!expr)
     {
-        printf("Test FAIL:%s\n", msg);
+        printf("FAILES: %s", msg);
     }
     else
     {
-        printf("passes.\n");
+        printf("passes");
     }
 }
