ef87f4a201200188aa79aeb1c98d4fcd6405d360
Daniel Wong <wongdan@onid.oregonstate.edu>
2015-05-11 23:13:13 -0700

    Assign 3
    Final Submit

--- wongdan/dominion/randomtestcard.c
+++ wongdan/dominion/randomtestcard.c
@@ -10,63 +10,68 @@
 #define MAX_TESTS 20
 
 int main() {
-
+	
 	int i, j, numPlayers, currPlayer, handCount, deckCount, discardCount, seed, numAction;
 	int fails = 0;
 
-	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
-		sea_hag, tribute, smithy};
+	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
 
-	struct gameState* G = newGame();
+	struct gameState state;
 	
-	printf("***** Starting Random Test for Village *****\n\n");
+	printf("Starting Test for Village");
 
 	for (i = 0; i < MAX_TESTS; i++) {
-		
-		printf("*** Test %d ***\n", i + 1);
-		numPlayers = 2 + (rand() % 3);
+		printf("*** Test %d ***\n", (i + 1));
+		numPlayers = 2 +(rand() % 3);
 
 		currPlayer = rand() % numPlayers;
 		seed = rand();
 
-		j = initializeGame(numPlayers, k, seed, G);
+		j = initializeGame(numPlayers, k, seed, &state);
 		assert(j == 0);
-		G->whoseTurn = currPlayer;
+		state.whoseTurn = currPlayer;
 
-		G->deckCount[currPlayer] = 10 + (rand() % MAX_DECK);
-		deckCount = G->deckCount[currPlayer];
+		state.deckCount[currPlayer] = (rand() % MAX_DECK);
+		state.discardCount[currPlayer] =(rand() % MAX_DECK);
+		state.handCount[currPlayer] = (rand() % MAX_HAND);
 
-		G->discardCount[currPlayer] = rand() % (deckCount);
-		discardCount = G->discardCount[currPlayer];
-
-		G->handCount[currPlayer] = rand() % MAX_HAND;
-		handCount = G->handCount[currPlayer];
-		numAction = G->numActions;
-
-		j = cardEffect(village, 1, 1, 1, G, 0, 0);
-
-		//Increase fails
-		if (j != 0){
-			fails++;
-			printf("cardEffect has returned wrong value!\n");
+		if (seed % 3 == 0) {
+			state.deckCount[currPlayer] = 0;
 		}
 
-		if (handCount != G->handCount[currPlayer]){
+		discardCount = state.handCount[currPlayer];
+		deckCount = state.deckCount[currPlayer];
+		handCount = state.handCount[currPlayer];
+		numAction = state.numActions;
+
+		printf("Cards discarded: %d\n", discardCount);
+		printf("Cards in deck: %d\n", deckCount);
+		printf("Cards in hand: %d\n", handCount);
+		printf("Actions left: %d\n", numAction);
+
+		j = cardEffect(village, 0, 0, 0, &state, 0, 0);
+
+		if (j != 0) {
 			fails++;
-			printf("Current player did not discard and draw.\n");
+			printf("cardEffect has return wrong value!\n");	
 		}
 
-		if (numAction + 2 != G->numActions) {
+		if (handCount != state.handCount[currPlayer]){
 			fails++;
-			printf("numActions did not increase by 1.\n");
+			printf("currPlayer did not discard and draw!\n");
+		}
+
+		if ((numAction + 2) != state.numActions) {
+			fails++;
+			printf("numActions did not increase by 1!\n");
 		}
 
 		else {
-			printf("Pass\n");
+			printf("Pass!\n");
 		}
 
 	}
-	printf("***** Test Conplete: %d/%d fails! *****\n\n\n", fails,MAX_TESTS);
+	printf("***** Test Complete! %d/%d tests fail! *****\n\n\n", fails,MAX_TESTS);
 
-return 0;
+return 0;	
 }
