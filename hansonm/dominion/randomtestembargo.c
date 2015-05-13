1bd7bdb3bd49f0383399ce4c5045a5ce84bfe9d2
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-05-13 14:56:28 -0700

    Submitted by email

--- hansonm/dominion/randomtestembargo.c
+++ hansonm/dominion/randomtestembargo.c
@@ -21,8 +21,6 @@ int main (int argc, char* argv[])
 		
 		initializeGame(players, k, seed, &G);
 		
-		//G.whoseTurn = player;
-		
 		G.deckCount[player] = rand() % MAX_DECK;
 		G.discardCount[player] = rand() % MAX_DECK;
 		G.handCount[player] = rand() % MAX_HAND;
@@ -31,21 +29,18 @@ int main (int argc, char* argv[])
 		G.numActions = rand();
 		coin_bonus = rand();
 		
-		int test_handcount = G.handCount[0] + 1; //Two cards were added
-		int test_coins = G.coins; // No extra coins
+		int test_handcount = G.handCount[0] - 1; //No cards were added
+		int test_coins = G.coins + 2; // two extra coins
 		int test_numbuys = G.numBuys; // No extra buys
 		int test_decksize = G.deckCount[0]; //No cards were drawn
 		int test_actioncount = G.numActions; //Doesn't count as a action
-		//int test_discardcount = G.discardCount[G.whoseTurn]; //card is played
-		printf("Discard count is %i --",G.discardCount[player]);
-		cardEffect(adventurer, 1, 1, 1, &G, G.handCount[player], &coin_bonus);
-		printf("Discard count now is %i\n",G.discardCount[player]);
-		if(G.handCount[0] != test_handcount) printf("Hand count is off: %i should be %i\n", test_handcount, G.handCount[0]);
-		//if(G.discardCount[0] != test_discardcount) printf("Discard count is off: %i should be %i\n", test_discardcount, G.discardCount[player]);
-		if(G.coins != test_coins) printf("Coins count is off\n");
 		
+		cardEffect(embargo, 1, 1, 1, &G, G.handCount[player], &coin_bonus);
+		
+		if(G.handCount[0] != test_handcount) printf("Hand count is off\n");
+		if(G.coins != test_coins) printf("Coins count is off\n");
 		if(G.numBuys != test_numbuys) printf("Buy count count is off\n");
-		//if(G.deckCount[0] != test_decksize) printf("Deck count count is off");
+		if(G.deckCount[0] != test_decksize) printf("Deck count count is off\n");
 		if(G.numActions != test_actioncount) printf("Action count count is off\n");
 
 	}
