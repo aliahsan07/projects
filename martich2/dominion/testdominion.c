98cf4591590765342b23492d59d19aa7a1c0dfe2
Christopher Martin <martich2@onid.orst.edu>
2015-05-19 15:21:54 -0700

    Added code to generate random unique kingdom cards Added comments to card enums

--- martich2/dominion/testdominion.c
+++ martich2/dominion/testdominion.c
@@ -17,9 +17,46 @@
 
 int main(int argc, char** argv)
 {
-    srand(time(NULL));          // init libc random seed
+    int kingdom_cards[10] = {0};
+    int *card_checklist;
+    int i = 0, j = 0;
+    int card = 0;
+
+
+	srand(time(NULL));          // init libc random seed
     int players = (rand() % 3) + 2; // 2 to 4 players
-	printf("Hello World: %d!\n", players);
+
+    card_checklist = calloc(sizeof(int) * 19); // 19 kingdom cards in our game.
+	assert(card_checklist == NULL);
+
+	// random unique list of kingdom cards
+	while(i < 10)
+	{
+		card = (rand() % 19); // 19 kingdom cards
+		if (card_checklist[card] == 0)
+		{
+			card_checklist[card] = (card + 7); // offset to cards 7 thru 26
+			i++;
+		}
+	}
+
+	// go thru the check list and assign the selected card to the card list
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; i < 19; j++)
+		{
+			if (card_checklist[j] != 0)
+			{
+				kingdom_cards[i] = card_checklist[j];
+				break;
+			}
+		}
+	}
+
+
+
+    printf("Hello World: %d!\n", players);
+    free(card_checklist);
 
 	return 0;
 }
