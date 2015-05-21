a7859e39180d570d97d4bd93d781c766f4de92cb
MrFengShui <luansongjian@outlook.com>
2015-05-21 08:55:54 -0700

    Last Commit

--- luans/dominion/testdominion.c
+++ luans/dominion/testdominion.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <time.h>
 
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include "rngs.h"
+#include "assert.h"
+#include "interface.h"
 
 void randomCards(int k[10])
 {
@@ -41,71 +44,78 @@ int main(int argc, char *argv[])
 	
 	int seed = rand() % 100;
 	int player_num = rand() % 3 + 2;
-	int i, x = 0;
+	int i;
 	char card_name[64];
 	
 	srand(time(0));
 
 	printf("Starting to test complete dominion ......\n");
-	printf("Numbers of players: %d\n", player_num);
-	printf("$$$$$ Printing the kingdom cards $$$$$\n");
+	printf("\n$$$$$ Starting to show entire process of playing this Dominion Game $$$$$\n\n");
 	
-	for(i = 0; i < 10; i ++)
+	if(strcmp(argv[1], "--test") == 0)
 	{
-		cardNumToName(k[i], card_name);
-		printf("Kindom card %d: %s and the value: %d\n", i + 1, card_name, k[i]);
-	}
-	
-	printf("Initializing dominion ......\n");
-	initializeGame(player_num, k, seed, &state);
-	
-	while(!isGameOver(&state))
-	{
-		int afterDraw, afterBuy;
-		int choice = rand() % 3;
-		int hand_count = game.handCount[game.whoseTurn];
-		int card_draw;
-                        
-		cardNumToName(hand_count, card_name);
+		printf("Numbers of players: %d\n", player_num);	
 		
-		switch(choice)
+		for(i = 0; i < 10; i ++)
 		{
-			case 0: 
-				{
-					card_draw = hand_count;
-					cardNumToName(card_draw, card_name);
-					afterDraw = playCard(card_draw, 0, 0, 0, &state);					
-					print_info(state);
-				}
-				break;
-			case 1:
-				{
-					int add_card = rand() % 20;
-					afterBuy = buyCard(add_card, &state);
-					print_info(state);
-				}
-				break;
-			case 2:
-				{
-					printf("It's turn to next Player %d\n", state.whoseTurn);
-					endTurn(&state);
-				}
-				break;
-			default:
-				printf("!!!!! Choice Error !!!!!");
-				exit(EXIT_FAILURE);
-		}
-		
-		if(choice == 0 || choice == 1)
-		{
-			if(afterDraw == -1)
-				printf("Player %d is drawing card [%s]\n", state.whoseTurn, card_name);
-			else
-				printf("Turn to next player\n");
+			cardNumToName(k[i], card_name);
+			printf("Kingdom Card %d: %d (%s)\n", i + 1, k[i], card_name);
 		}
 	}
 	
-	print_info(&state);
+	if(strcmp(argv[1], "--game") == 0)
+	{
+		printf("\nInitializing dominion ......\n\n");
+		initializeGame(player_num, k, seed, &state);
+			
+		while(!isGameOver(&state))
+		{
+			int after_draw, after_buy, add_card;
+			int choice = rand() % 3;
+			int hand_count = state.handCount[state.whoseTurn];
+			int card_draw;
+								
+			cardNumToName(hand_count, card_name);
+				
+			switch(choice)
+			{
+				case 0: 
+					{
+						card_draw = hand_count;
+						cardNumToName(card_draw, card_name);
+						after_draw = playCard(card_draw, 0, 0, 0, &state);					
+						print_info(state);
+					}
+					break;
+				case 1:
+					{
+						add_card = rand() % 20;
+						after_buy = buyCard(add_card, &state);
+						print_info(state);
+					}
+					break;
+				case 2:
+					{
+						printf("It's turn to next Player %d\n", state.whoseTurn);
+						endTurn(&state);
+					}
+					break;
+				default:
+					printf("!!!!! Choice Error !!!!!");
+					exit(EXIT_FAILURE);
+			}
+				
+			if(choice == 0 || choice == 1)
+			{
+				if(after_draw == -1)
+					printf("Player %d is drawing card [%s]\n", state.whoseTurn, card_name);
+				else
+					printf("Turn to next player\n");
+			}
+		}
+			
+		print_info(state);
+	}
 	printf("Ending to test complete dominion ......\n");
 	
 	return EXIT_SUCCESS;
