92ace9d2043fa86d00f92a1747ff67b6619c4760
drkitty <bumblingkitty@gmail.com>
2015-05-24 14:06:06 -0700

    Only put diff in log if there are differences

--- pochisr/dominion/testdominion.sh
+++ pochisr/dominion/testdominion.sh
@@ -1,7 +1,15 @@
+temp=$(mktemp)
+
+trap "rm $temp" EXIT
+
 for (( i = 1; i <= 1000000; i++ )); do
 	echo '##################'
 	echo SEED = $i
 	echo '##################'
 
-	./fdiff <(./testdominion $i 2>&1) <(./testdominion_alt $i 2>&1) 10
+	./fdiff <(stdbuf -o0 ./testdominion $i 2>&1) \
+		<(stdbuf -o0 ./testdominion_alt $i 2>&1) 10 >$temp
+	if [[ $? == 1 ]]; then
+		cat $temp
+	fi
 done
