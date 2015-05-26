0d60565688867e24b538a1d3756cd56dbd0bd15d
ClownBabyJunior <john.calvin.coolidge@gmail.com>
2015-05-26 14:21:04 -0700

    Added a shell script to handle running the tests

---
+++ haagensa/dominion/testDomionDiff.sh
@@ -0,0 +1,10 @@
+cp myDominion.c dominion.c
+make clean
+make all
+make testdominion.out
+cp gameResults.out gameResults.my.out
+cp hansemikDominion.c dominion.c
+make clean
+make all
+make testdominion.out
+cp gameResults.out gameResults.other.out
