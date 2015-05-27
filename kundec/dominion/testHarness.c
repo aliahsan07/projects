751db9e2e3de428599fb5196527f916888883d53
Kunde21 <Kunde21@gmail.com>
2015-05-27 02:00:36 -0700

    Assignment 4
    Repairing the destruction.

---
+++ kundec/dominion/testHarness.c
@@ -0,0 +1,26 @@
+#include "dominion.h"
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "displayDom.h"
+#include "testdominion.c"
+
+int main(int argc, char *argv[]){
+
+	int i;
+
+	if (argc > 1){
+		if (sscanf(argv[1], "%i", &i) != 1){ i = MAX_CASES; }
+		if (i < MAX_CASES){ TestCase(i); }
+	}
+	else{
+		for (i = 0; i < MAX_CASES; ++i){
+			TestCase(i);
+		}
+	}
+
+	return 0;
+}
