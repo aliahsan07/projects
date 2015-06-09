cb899c2a30d0bb8d7d393fba0887f1be1f0e353a
EliasRademacher <rademace@onid.oregonstate.edu>
2015-06-09 00:10:33 -0700

    Have passing_test.c create standalone c file with function calls

--- rademace/dominion/MyDD.py
+++ rademace/dominion/MyDD.py
@@ -1,31 +1,52 @@
-# $Id: MyDD.py,v 1.1 2001/11/05 19:53:33 zeller Exp $
-# Template for adapting delta debugging.  Areas to customize are
-# tagged with `FIXME'.
-
 import DD
+import commands
+import string
+
 
 class MyDD(DD.DD):
     def __init__(self):
         DD.DD.__init__(self)
         
     def _test(self, deltas):
-	# FIXME: Set up a test function that takes a set of deltas and
+        # FIXME: Set up a test function that takes a set of deltas and
         # returns either self.PASS, self.FAIL, or self.UNRESOLVED.
-        if c == []:
+        # Build input
+        input = ""
+        for (index, character) in deltas:
+            input = input + character
+
+       
+        # Invoke dominion.c
+        (status, output) = commands.getstatusoutput(
+            "passing_test " + input)
+
+        #print "output", output
+        print "Exit Code", status
+
+        # Determine outcome
+        if status == 0:
             return self.PASS
+        elif string.find(output, "INVALID") >= 0:
+            return self.FAIL
         return self.UNRESOLVED
 
 if __name__ == '__main__':
-    deltas = [ 1 ]
-    # FIXME: Insert your deltas here
+    # FIXME: Insert your deltas here 
+     deltas = []
+    index = 1
+		f = open('failing_test.c', 'r')
+    for line in f:
+        deltas.append((index, line))
+        index = index + 1
 
+    	
     mydd = MyDD()
     
-    # print "Simplifying failure-inducing input..."
-    # c = mydd.ddmin(deltas)              # Invoke DDMIN
-    # print "The 1-minimal failure-inducing input is", c
-    # print "Removing any element will make the failure go away."
-    # print
+    print "Simplifying failure-inducing input..."
+    c = mydd.ddmin(deltas)              # Invoke DDMIN
+    print "The 1-minimal failure-inducing input is", c
+    print "Removing any element will make the failure go away."
+    print
     
     print "Isolating the failure-inducing difference..."
     (c, c1, c2) = mydd.dd(deltas)	# Invoke DD
@@ -34,7 +55,6 @@ if __name__ == '__main__':
 
 
 
-
 # Local Variables:
 # mode: python
 # End:
